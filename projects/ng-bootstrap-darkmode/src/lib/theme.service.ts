14d2e77af25d05d24e196f240ea9fa62588bd71b
Adrian Kunz <clashsoft@hotmail.com>
2020-06-07 13:16:33 +0200

    Add simple theme service implementation

--- projects/ng-bootstrap-darkmode/src/lib/theme.service.ts
+++ projects/ng-bootstrap-darkmode/src/lib/theme.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  private _theme = new BehaviorSubject<string | null>(null);
 
-  constructor() { }
+  constructor() {
+  }
+
+  get theme$(): Observable<string> {
+    return this._theme;
+  }
+
+  get theme(): string | null {
+    return this._theme.getValue();
+  }
+
+  set theme(value: string | null) {
+    this._theme.next(value);
+  }
 }
