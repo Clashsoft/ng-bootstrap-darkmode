780d508d6f25a4f520959958771a6415de66c85d
Adrian Kunz <clashsoft@hotmail.com>
2020-07-18 12:33:45 +0200

    Implement detection, saving and loading in theme service

--- projects/ng-bootstrap-darkmode/src/lib/theme.service.ts
+++ projects/ng-bootstrap-darkmode/src/lib/theme.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ThemeService {
+export class ThemeService implements OnInit {
   private _theme = new BehaviorSubject<string | null>(null);
+
+  loadHandler: () => string | null = () => localStorage.getItem('theme') ?? null;
+  saveHandler: (theme: string | null) => void = theme => {
+    if (theme) {
+      localStorage.setItem('theme', theme);
+    } else {
+      localStorage.removeItem('theme');
+    }
+  }
 
   constructor() {
   }
 
+  ngOnInit(): void {
+    this._theme.next(this.savedTheme ?? this.detectedTheme);
+    this._theme.subscribe(theme => {
+      if (theme) {
+        document.body.setAttribute('theme', theme);
+      } else {
+        document.body.removeAttribute('theme');
+      }
+    });
+    this._theme.subscribe(theme => this.savedTheme = theme);
+  }
+
   get theme$(): Observable<string | null> {
     return this._theme;
+  }
+
+  get detectedTheme(): string {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
   }
 
   get theme(): string | null {
@@ -20,5 +45,13 @@ export class ThemeService {
 
   set theme(value: string | null) {
     this._theme.next(value);
+  }
+
+  get savedTheme(): string | null {
+    return this.loadHandler();
+  }
+
+  set savedTheme(theme: string | null) {
+    this.saveHandler(theme);
   }
 }
