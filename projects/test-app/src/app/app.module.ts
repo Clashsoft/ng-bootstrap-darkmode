c8092df82a032ec21a7ea04df3bc3cd7f71222bc
Adrian Kunz <clashsoft@hotmail.com>
2020-09-05 13:45:54 +0200

    test: Implement test app

--- projects/test-app/src/app/app.module.ts
+++ projects/test-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgBootstrapDarkmodeModule} from 'ng-bootstrap-darkmode';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgBootstrapDarkmodeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
