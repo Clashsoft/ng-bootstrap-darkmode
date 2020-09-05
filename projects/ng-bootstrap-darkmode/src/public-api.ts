304a5ea274f898e6128941df251fe2d7c72fbd66
Adrian Kunz <clashsoft@hotmail.com>
2020-09-05 13:26:38 +0200

    fix: Update public-api.ts

--- projects/ng-bootstrap-darkmode/src/public-api.ts
+++ projects/ng-bootstrap-darkmode/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ng-bootstrap-darkmode
  */
 
-export * from './lib/ng-bootstrap-darkmode.service';
-export * from './lib/ng-bootstrap-darkmode.component';
+export * from './lib/theme.service';
+export * from './lib/dark-switch/dark-switch.component';
 export * from './lib/ng-bootstrap-darkmode.module';
