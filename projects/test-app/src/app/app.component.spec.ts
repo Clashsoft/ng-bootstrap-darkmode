96b5082aa01ba72abbc853d9ae77eaed470896e4
Adrian Kunz <clashsoft@hotmail.com>
2020-09-06 12:42:42 +0200

    test: FIx app.component.spec.ts

--- projects/test-app/src/app/app.component.spec.ts
+++ projects/test-app/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('test-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-app app is running!');
-  });
 });
